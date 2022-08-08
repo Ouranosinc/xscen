177830c52eb4e242ee8acbe2dc9bf5f1e0d30382
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2022-08-08 15:40:59 -0400

    FYI if your class does not being with `Test`, pytest ignores it

--- tests/test_xscen.py
+++ tests/test_xscen.py
@@ -3,11 +3,11 @@
 from pathlib import Path
 
 
-class SmokeTests:
+class TestSmokeTest:
     def test_version(self):
         import xscen
 
-        with Path().cwd().parent.joinpath("setup.cfg").open() as reader:
+        with Path(__file__).parent.parent.joinpath("setup.cfg").open() as reader:
             for line in reader.readlines():
                 if line.startswith("current_version"):
                     assert xscen.__version__ == line.split()[-1]
