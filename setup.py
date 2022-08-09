2ed21089bc051c522ad9393f6df59213126ec409
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2022-08-09 13:03:57 -0400

    update dev recipe to include pytest-cov, update CONTRIBUTING.rst to include warning about need for tox-conda, update environment-dev.yml to include tox and tox-conda, fix typo

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     name="xscen",
     packages=find_packages(include=["xscen", "xscen.*"]),
     test_suite="tests",
-    tests_require=["pytest"],
+    tests_require=["pytest", "pytest-cov"],
     url="https://github.com/Ouranosinc/xscen",
     version="0.2.5-beta",
     zip_safe=False,
