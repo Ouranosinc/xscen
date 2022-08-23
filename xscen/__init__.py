994b936390e5eefbad2c8d960be89cbe45d93123
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2022-08-23 16:39:35 -0400

    bumpversion relase

--- xscen/__init__.py
+++ xscen/__init__.py
@@ -38,7 +38,7 @@ from .utils import clean_up
 
 __author__ = """Gabriel Rondeau-Genesse"""
 __email__ = "rondeau-genesse.gabriel@ouranos.ca"
-__version__ = "0.3.2-beta"
+__version__ = "0.3.2"
 
 
 # monkeypatch so that warnings.warn() doesn't mention itself
