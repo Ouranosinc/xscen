97dd92b90a8e19d7c3df6a43dc31ae253c2b283d
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2022-09-08 15:51:27 -0400

    Add logo files and logos to docs and README.rst

--- docs/conf.py
+++ docs/conf.py
@@ -159,12 +159,16 @@ html_theme = "sphinx_rtd_theme"
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {"logo_only": True, "style_external_links": True}
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = "_static/_images/xclim-logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ---------------------------------------
