adbfa3fa88e3629b9b6a52a1aec794416f3b410a
RondeauG <rondeau-genesse.gabriel@ouranos.ca>
2023-05-04 16:01:42 -0400

    don't import cftime

--- xscen/aggregate.py
+++ xscen/aggregate.py
@@ -7,7 +7,6 @@ from pathlib import Path, PosixPath
 from types import ModuleType
 from typing import Sequence, Tuple, Union
 
-import cftime
 import geopandas as gpd
 import numpy as np
 import pandas as pd
