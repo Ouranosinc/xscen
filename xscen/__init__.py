85ee4f3473b029b9b28580120e8ee8327f1bad96
RondeauG <rondeau-genesse.gabriel@ouranos.ca>
2023-05-31 13:35:10 -0400

    get_warming_level

--- xscen/__init__.py
+++ xscen/__init__.py
@@ -26,7 +26,12 @@ from .catalog import DataCatalog, ProjectCatalog  # noqa
 from .config import CONFIG, load_config  # noqa
 from .diagnostics import properties_and_measures
 from .ensembles import *
-from .extract import extract_dataset, search_data_catalogs, subset_warming_level  # noqa
+from .extract import (  # noqa
+    extract_dataset,
+    get_warming_level,
+    search_data_catalogs,
+    subset_warming_level,
+)
 from .indicators import compute_indicators  # noqa
 from .io import save_to_netcdf, save_to_zarr  # noqa
 from .reduce import build_reduction_data, reduce_ensemble
