66a03258e93bf91e6696e0f52b37d124102187a0
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2023-11-27 15:04:53 -0500

    MANIFEST.in fixes, publish_release_notes update, re-add xdoctest, saner tox builds

--- xscen/utils.py
+++ xscen/utils.py
@@ -935,16 +935,21 @@ def clean_up(  # noqa: C901
 
 
 def publish_release_notes(
-    style: str = "md", file: Optional[Union[os.PathLike, StringIO, TextIO]] = None
+    style: str = "md",
+    file: Optional[Union[os.PathLike, StringIO, TextIO]] = None,
+    changes: Union[str, os.PathLike] = None,
 ) -> Optional[str]:
     """Format release history in Markdown or ReStructuredText.
 
     Parameters
     ----------
-    style: {"rst", "md"}
-      Use ReStructuredText formatting or Markdown. Default: Markdown.
-    file: {os.PathLike, StringIO, TextIO}, optional
-      If provided, prints to the given file-like object. Otherwise, returns a string.
+    style : {"rst", "md"}
+        Use ReStructuredText (`rst`) or Markdown (`md`) formatting. Default: Markdown.
+    file : {os.PathLike, StringIO, TextIO, None}
+        If provided, prints to the given file-like object. Otherwise, returns a string.
+    changes : {str, os.PathLike}, optional
+        If provided, manually points to the file where the changelog can be found.
+        Assumes a relative path otherwise.
 
     Returns
     -------
@@ -955,13 +960,16 @@ def publish_release_notes(
     This function exists solely for development purposes.
     Adapted from xclim.testing.utils.publish_release_notes.
     """
-    history_file = Path(__file__).parent.parent.joinpath("HISTORY.rst")
+    if isinstance(changes, (str, Path)):
+        changes_file = Path(changes).absolute()
+    else:
+        changes_file = Path(__file__).absolute().parents[2].joinpath("CHANGES.rst")
 
-    if not history_file.exists():
-        raise FileNotFoundError("History file not found in xscen file tree.")
+    if not changes_file.exists():
+        raise FileNotFoundError("Changes file not found in xscen file tree.")
 
-    with open(history_file) as hf:
-        history = hf.read()
+    with open(changes_file) as f:
+        changes = f.read()
 
     if style == "rst":
         hyperlink_replacements = {
@@ -979,32 +987,34 @@ def publish_release_notes(
         raise NotImplementedError()
 
     for search, replacement in hyperlink_replacements.items():
-        history = re.sub(search, replacement, history)
+        changes = re.sub(search, replacement, changes)
 
     if style == "md":
-        history = history.replace("=======\nHistory\n=======", "# History")
+        changes = changes.replace("=========\nChangelog\n=========", "# Changelog")
 
         titles = {r"\n(.*?)\n([\-]{1,})": "-", r"\n(.*?)\n([\^]{1,})": "^"}
         for title_expression, level in titles.items():
-            found = re.findall(title_expression, history)
+            found = re.findall(title_expression, changes)
             for grouping in found:
                 fixed_grouping = (
                     str(grouping[0]).replace("(", r"\(").replace(")", r"\)")
                 )
                 search = rf"({fixed_grouping})\n([\{level}]{'{' + str(len(grouping[1])) + '}'})"
                 replacement = f"{'##' if level=='-' else '###'} {grouping[0]}"
-                history = re.sub(search, replacement, history)
+                changes = re.sub(search, replacement, changes)
 
         link_expressions = r"[\`]{1}([\w\s]+)\s<(.+)>`\_"
-        found = re.findall(link_expressions, history)
+        found = re.findall(link_expressions, changes)
         for grouping in found:
             search = rf"`{grouping[0]} <.+>`\_"
             replacement = f"[{str(grouping[0]).strip()}]({grouping[1]})"
-            history = re.sub(search, replacement, history)
+            changes = re.sub(search, replacement, changes)
 
     if not file:
-        return history
-    print(history, file=file)
+        return
+    if isinstance(file, (Path, os.PathLike)):
+        file = Path(file).open("w")
+    print(changes, file=file)
 
 
 def unstack_dates(
