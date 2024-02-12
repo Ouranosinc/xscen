093bc262da07e9a4eb4483d737a0063ade225723
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2024-02-12 15:03:54 -0500

    Test 1

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ class InstallWithCompile(install):
 
     def run(self):
         """Install the package, but compile the i18n catalogs first."""
+        with open("/home/pbourg/Projects/xscen/wtf.log", "w") as f:
+            f.write(str(self.distribution))
         compiler = compile_catalog(self.distribution)
         option_dict = self.distribution.get_option_dict("compile_catalog")
         compiler.domain = [option_dict["domain"][1]]
