560b13bb70e1c62be64f0e1c077fd0177199f70a
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2024-02-27 15:48:42 -0500

    Bump version: 0.8.3-dev.0 → 0.8.3

--- xscen/__init__.py
+++ xscen/__init__.py
@@ -53,7 +53,7 @@ from .utils import clean_up
 
 __author__ = """Gabriel Rondeau-Genesse"""
 __email__ = "rondeau-genesse.gabriel@ouranos.ca"
-__version__ = "0.8.3-dev.0"
+__version__ = "0.8.3"
 
 
 def warning_on_one_line(
