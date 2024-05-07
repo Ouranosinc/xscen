a29eacc0d69cd190e760ec1fb58c227bca822145
RondeauG <rondeau-genesse.gabriel@ouranos.ca>
2024-05-07 13:30:55 -0400

    Bump version: 0.8.4-dev.14 → 0.9.0-dev.0

--- xscen/__init__.py
+++ xscen/__init__.py
@@ -51,7 +51,7 @@ from .utils import clean_up
 
 __author__ = """Gabriel Rondeau-Genesse"""
 __email__ = "rondeau-genesse.gabriel@ouranos.ca"
-__version__ = "0.8.4-dev.14"
+__version__ = "0.9.0-dev.0"
 
 
 def warning_on_one_line(
