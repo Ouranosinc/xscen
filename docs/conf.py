400cb7e78b65b5bb83e1a691fb7445aa9a74d1d0
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2024-09-11 15:01:53 -0400

    fast-forward cookiecutter

--- docs/conf.py
+++ docs/conf.py
@@ -141,9 +141,7 @@ linkcheck_ignore = [
 # templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
+# You can specify multiple suffix as a dictionary of suffix: filetype
 source_suffix = {'.rst': 'restructuredtext'}
 
 # The master toctree document.
