fbf2fff21f8397d2577e444a40c67cdcc02088c7
RondeauG <rondeau-genesse.gabriel@ouranos.ca>
2024-10-07 16:27:22 -0400

    another one

--- src/xscen/extract.py
+++ src/xscen/extract.py
@@ -37,7 +37,7 @@ logger = logging.getLogger(__name__)
 
 __all__ = [
     "extract_dataset",
-    "get_horizon_from_warming_level",
+    "get_period_from_warming_level",
     "get_warming_level",
     "get_warming_level_from_period",
     "resample",
