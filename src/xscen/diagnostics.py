4148cdea555fb257119c5a33c2c00f9f121afb66
Éric Dupuis <eric.dupuis.1@umontreal.ca>
2024-10-23 14:38:54 -0400

    import Union

--- src/xscen/diagnostics.py
+++ src/xscen/diagnostics.py
@@ -7,6 +7,8 @@ from collections.abc import Sequence
 from copy import deepcopy
 from pathlib import Path
 from types import ModuleType
+from typing import Union
+
 
 import numpy as np
 import xarray as xr
