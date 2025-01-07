9ecc8c90dad1f942b7757f707917983c50d91167
juliettelavoie <juliette.lavoie@hotmail.ca>
2025-01-07 14:44:49 -0500

    pin xarray

--- src/xscen/io.py
+++ src/xscen/io.py
@@ -1053,7 +1053,7 @@ def rechunk(
         raise ValueError(
             "No chunks given. Need to give at `chunks_over_var` or `chunks_over_dim`."
         )
-
+    print(ds, chunks, worker_mem, str(path_out), str(temp_store))
     plan = _rechunk(ds, chunks, worker_mem, str(path_out), temp_store=str(temp_store))
 
     plan.execute()
