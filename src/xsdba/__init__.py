4f314e7dacab9fbdc32552ed9184cb8e857e9b6e
Éric Dupuis <eric.dupuis.1@umontreal.ca>
2025-01-09 10:31:38 -0500

    Bump version: 0.1.1-dev.1 → 0.2.0-dev.0

--- src/xsdba/__init__.py
+++ src/xsdba/__init__.py
@@ -47,4 +47,4 @@ if hasattr(adjustment, "_generate_SBCK_classes"):
 
 __author__ = """Trevor James Smith"""
 __email__ = "smith.trevorj@ouranos.ca"
-__version__ = "0.1.1-dev.1"
+__version__ = "0.2.0-dev.0"
