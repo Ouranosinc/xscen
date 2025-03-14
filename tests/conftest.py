2b525b2206d84d26da78ce6a253b627281adb0f2
RondeauG <rondeau-genesse.gabriel@ouranos.ca>
2025-03-14 14:32:53 -0400

    try 8.7.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 import pytest
 
-import h5py  # To fix a bug with ESMPy in pure 'pip install' environments
 import xscen as xs
 from xscen.testing import datablock_3d as _datablock_3d
 
