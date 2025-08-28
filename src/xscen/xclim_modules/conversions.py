4f89d1563d3461a365b5dbdda02b57eced509fd5
Pascal Bourgault <bourgault.pascal@ouranos.ca>
2025-08-28 13:56:22 -0400

    Fix - this time i tested it

--- src/xscen/xclim_modules/conversions.py
+++ src/xscen/xclim_modules/conversions.py
@@ -7,11 +7,9 @@ from xclim.core.units import convert_units_to, declare_units
 from xsdba.processing import from_additive_space, to_additive_space
 
 try:
-    from xclim.indicators.convert import (
-        mean_temperature_from_max_and_min as tas_midpoint,
-    )
+    from xclim.indices.converters import tas_from_tasmin_tasmax as tas_midpoint
 except ImportError:  # FIXME: Remove when we pin xclim >= 0.58
-    from xclim.indicators.atmos import tg as tas_midpoint
+    from xclim.indices import tas as tas_midpoint
 
 
 @declare_units(prsn="[precipitation]", prlp="[precipitation]")
