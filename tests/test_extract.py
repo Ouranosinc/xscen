f6b8d43858003397b54a2f079cda13057e702f06
Juliette Lavoie <juliette.lavoie@hotmail.ca>
2025-11-05 09:23:08 -0500

    testing where it fails

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -671,13 +671,14 @@ class TestExtractDataset:
             variables_and_freqs={"tasmax": "D", "tasmin": "D"},
             other_search_criteria={"experiment": ["ssp126"]},
         ).popitem()[1]
+        print(out)
 
-        # should return without error because time has been floored
-        ds_sim = xs.extract_dataset(catalog=out)
+        # # should return without error because time has been floored
+        # ds_sim = xs.extract_dataset(catalog=out)
 
-        assert len(ds_sim["D"].time.values) == 730
+        # assert len(ds_sim["D"].time.values) == 730
 
-        # twice as much time because we have time at 00:00 and 12:00 without correction
-        ds_sim = xs.extract_dataset(catalog=out, ensure_correct_time=False)
+        # # twice as much time because we have time at 00:00 and 12:00 without correction
+        # ds_sim = xs.extract_dataset(catalog=out, ensure_correct_time=False)
 
-        assert len(ds_sim["D"].time.values) == 1460
+        # assert len(ds_sim["D"].time.values) == 1460
