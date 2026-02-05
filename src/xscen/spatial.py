498162e2029d738ded54cec7037396f4fa312bfd
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2026-02-05 14:26:41 -0500

    Add Region to __all__, move docstring to typevar
    Signed-off-by: Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>

--- src/xscen/spatial.py
+++ src/xscen/spatial.py
@@ -42,6 +42,7 @@ from .config import parse_config
 logger = logging.getLogger(__name__)
 
 __all__ = [
+    "Region",
     "creep_fill",
     "creep_weights",
     "dataset_extent",
@@ -55,6 +56,25 @@ __all__ = [
 
 
 Region = TypeVar("Region", bound=dict)
+"""
+A region specification, a dictionary with the following valid entries:
+
+- ``name`` : region name for the `domain` column
+- ``method`` : the method used for subsetting the region from a dataset:
+    - ``"shape"`` : The region is defined by a polygon in entry "shape".
+    - ``"bbox"`` : The region is defined by bounds in entries "lat_bnds" and "lon_bnds".
+    - ``"sel"`` : The region is defined by bounds in entries with the same name as the spatial coordinates.
+    - ``"gridpoint"`` : The region is a list of points defined in entries "lon" and "lat"
+
+- ``tile_buffer`` : For methods "bbox" and "shape", the actual subset is enlarged by this many grid cell sizes (approximated).
+  This differs from clisops\' ``buffer`` argument in :py:func:`~clisops.core.subset.subset_shape`.
+- ``shape`` : For method "shape" only. A shapely Polygon, geopandas GeoDataFrame or GeoSeries, or a path to a file that GeoPandas can open.
+  When multiplie shapes are present, the subsetting is done with the unary union of the geometries.
+  When the CRS information is missing, EPSG 4326 is assumed.
+- ``lat_bnds`` and ``lon_bnds`` : For method "bbox" only. Tuples of the minimum and maximum latitude or longitude values.
+- ``lon`` and ``lat`` : For method "gridpoint", 1D arrays of the longitude and latitude coordinates of the points to extract.
+- ``<coordinate name>`` : For method "sel", any other entry is understood as a tuple of the bounds to extract for that spatial dimension.
+"""
 
 
 @parse_config
